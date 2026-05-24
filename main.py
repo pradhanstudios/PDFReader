1f6c3d4b2da43d334543e21609c606df5c0215af
Samik Pradhan <samikpradhan@gmail.com>
2026-05-24 12:36:51 -0700

    PDF upload shows text in Tkinter window w/ scroll

--- main.py
+++ main.py
@@ -8,6 +8,22 @@ window = tk.Tk()
 window.title("PDF Reader")
 window.geometry("1280x720")
 
+# Text Area Frame
+frame = tk.Frame(window)
+frame.pack(padx=10, pady=10, expand=True, fill=tk.BOTH)
+
+# Vertical Scrollbar
+v_scrollbar = tk.Scrollbar(frame)
+v_scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+# Text Box
+textBox = tk.Text(frame, height=8)
+textBox.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+
+# Scrollbar Config
+textBox["yscrollcommand"] = v_scrollbar.set
+v_scrollbar.config(command=textBox.yview)
+
 
 def upload_file():
     supported_file_types = [("PDF Files", "*.pdf")]
@@ -17,7 +33,8 @@ def upload_file():
         # process file here
         reader = PdfReader(file_path)
         text = "\n".join(p.extract_text() for p in reader.pages)
-        print(text[:10000])
+        textBox.insert(index="1.0", chars=text)
+        # print(text[:10000])
 
 
 def handle_button_press():
